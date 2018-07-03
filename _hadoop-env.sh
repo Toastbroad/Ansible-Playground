8a2e558ce37dcd84aeab8bf654cfd5d909f056a4
Broad, Oliver <Oliver.Broad@aperto.com>
2018-07-03 14:49:15 +0200

    wip

--- _hadoop-env.sh
+++ _hadoop-env.sh
@@ -419,4 +419,8 @@ esac
 # It uses the format of (command)_(subcommand)_USER.
 #
 # For example, to limit who can execute the namenode command,
-# export HDFS_NAMENODE_USER=hdfs
+export HDFS_NAMENODE_USER="root"
+export HDFS_DATANODE_USER="root"
+export HDFS_SECONDARYNAMENODE_USER="root"
+export YARN_RESOURCEMANAGER_USER="root"
+export YARN_NODEMANAGER_USER="root"
